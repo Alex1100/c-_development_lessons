82e14cd2c6f10eb249f16592eb20a1202de0a45c
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-05-17 12:58:19 -0700

    add pointer_arithmetic

---
+++ pointer_arithmetic.cpp
@@ -0,0 +1,20 @@
+/*
+  Subtracting two pointer variables
+  after literal subtraction, the result
+  will be divided by the size of the
+  data type
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int arr[] = {100, 200, 90, 800, 1000};
+  int z = &arr[4] - &arr[2];
+  printf("z = %d\n", z);
+
+  double amount[] = {100.0, 200.15, 9000.99, 1000.11, 2500.55};
+  z = &amount[4] - &amount[0];
+  printf("z = %d\n", z);
+  return 0;
+}
