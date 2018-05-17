59fb34b566263fb085f20f49bc99751bc11b10c0
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-05-17 16:58:44 -0700

    add realloc to malloc

--- malloc_example.cpp
+++ malloc_example.cpp
@@ -18,5 +18,22 @@ int main()
     printf("arr1[%d] = %d\n", k, *(arr1 + k));
   }
 
+  arr1 = (int*) realloc(arr1, sizeof(int) * 10);
+  printf("\n__________________\n\n");
+
+  for (int a = z; a < (z + 10); ++a)
+  {
+    printf("arr1[%d] = ", a);
+    scanf("%d", &arr1[a]);
+  }
+
+  printf("\n_____________\n\n");
+
+  for (int x = 0; x < z + 10; ++x)
+  {
+    printf("\n%d\t", *(arr1 + x));
+  }
+
+  printf("\n");
   return 0;
 }
