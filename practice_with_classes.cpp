f454ca3cd01da464f63b968a13bec9ac91358aae
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-05-17 21:15:50 -0700

    add basic c++ class exercise

---
+++ practice_with_classes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class employees {
+  long eocode;
+  int bsal;
+  float hra, pf;
+  public:
+    void assign(long _eocode, int _bsal, float _hra, float _pf) {
+      eocode = _eocode;
+      bsal = _bsal;
+      hra = _hra;
+      pf = _pf;
+    };
+
+    float calc_netsalary()
+    {
+      float nsal = bsal + hra - pf;
+      return nsal;
+    };
+};
+
+
+int main() {
+  employees eo1, eo2, eo3;
+  eo1.assign(101, 10000, 1000.11, 900.99);
+  eo2.assign(101, 10000, 1000.55, 900.11);
+  eo3.assign(101, 10000, 1000, 9000);
+  cout << endl << "NET SAL OF FIRST EMPLOYEE IS = " << eo1.calc_netsalary() << endl << "NET SAL OF SECOND EMPLOYEE IS = " << eo2.calc_netsalary() << endl << "NET SAL OF THIRD EMPLOYEE IS = " << eo3.calc_netsalary() << endl;
+}
